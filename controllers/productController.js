a8c6c37c9132ee3be52fd3f35aad600ed4137661
primawk <pwkuntoro@gmail.com>
2022-04-17 20:06:36 +0700

    fixed typo

--- controllers/productController.js
+++ controllers/productController.js
@@ -129,7 +129,7 @@ module.exports = {
       );
       res.status(200).send("Product edited successfully!");
     } catch (error) {
-      res.status(500).send(err);
+      res.status(500).send(error);
     }
   },
 
