ff53a947b6248196a4e6850c5fa692518fd0e63d
randhysatrio <randhysatrio@gmail.com>
2022-04-25 15:11:38 +0700

    Added additional controller for Address Controller

--- routers/addressRouter.js
+++ routers/addressRouter.js
@@ -1,8 +1,11 @@
 const { addressController } = require('../controllers');
 const router = require('express').Router();
+const { verifyToken } = require('../configs/jwtuser');
 
-router.post('/add', addressController.add);
-router.get('/find/:id', addressController.get);
-router.delete('/delete/:id', addressController.delete);
+router.post('/add', verifyToken, addressController.add);
+router.patch('/update/:id', verifyToken, addressController.update);
+router.patch('/edit/:id', verifyToken, addressController.edit);
+router.get('/find', verifyToken, addressController.get);
+router.delete('/delete/:id', verifyToken, addressController.delete);
 
 module.exports = router;
