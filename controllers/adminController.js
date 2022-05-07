636af8216b1b1937ec5b6fbce6e5907f6d18edf6
randhysatrio <randhysatrio@gmail.com>
2022-05-07 20:30:08 +0700

    Finalized backend functionality for Managed Admin Page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -35,29 +35,56 @@ module.exports = {
 
       const { rows, count } = await Admin.findAndCountAll(query);
 
+      const totalAdmins = await Admin.count();
+
       const maxPage = Math.ceil(count / limit);
 
-      res.status(200).send({ rows, count, maxPage });
+      res.status(200).send({ rows, count, maxPage, totalAdmins });
     } catch (err) {
       res.status(500).send(err);
     }
   },
   createAdmin: async (req, res) => {
     try {
-      req.body.password = Crypto.createHmac('sha1', 'hash123').update(req.body.password).digest('hex');
+      const { data, limit } = req.body;
 
-      const usernameCheck = await Admin.findOne({ where: { username: req.body.username } });
-      const emailCheck = await Admin.findOne({ where: { email: req.body.email } });
+      data.password = Crypto.createHmac('sha1', 'hash123').update(data.password).digest('hex');
+
+      const usernameCheck = await Admin.findOne({ where: { username: data.username } });
+      const emailCheck = await Admin.findOne({ where: { email: data.email } });
 
       if (usernameCheck) {
         res.send({ conflict: 'This username has already been used!' });
       } else if (emailCheck) {
-        res.send({ conflict: 'This username has already been used!' });
+        res.send({ conflict: 'This email has already been registered!' });
       } else {
-        await Admin.create(req.body);
+        await Admin.create(data);
 
-        res.status(201).send('Admin Account created successfully!');
+        const rows = await Admin.findAll({ limit });
+
+        const maxPage = Math.ceil(rows / limit);
+
+        const totalAdmins = await Admin.count();
+
+        res.status(201).send({ message: 'Admin account created successfully!', rows, maxPage, totalAdmins });
       }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
+  deleteAdmin: async (req, res) => {
+    try {
+      const { limit } = req.body;
+
+      await Admin.destroy({ where: { id: req.params.id } });
+
+      const rows = await Admin.findAll({ limit });
+
+      const maxPage = Math.ceil(rows / limit);
+
+      const totalAdmins = await Admin.count();
+
+      res.status(200).send({ message: 'Admin account deleted successfully!', rows, maxPage, totalAdmins });
     } catch (err) {
       res.status(500).send(err);
     }
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -3,5 +3,6 @@ const { adminController } = require('../controllers');
 
 router.post('/all', adminController.getAdmins);
 router.post('/create', adminController.createAdmin);
+router.post('/delete/:id', adminController.deleteAdmin);
 
 module.exports = router;
