9b91e246ef3125748d38736795cd4146a4c821ee
primawk <pwkuntoro@gmail.com>
2022-05-16 15:23:41 +0700

    dashboardTransaction search

--- controllers/transactionAdminController.js
+++ controllers/transactionAdminController.js
@@ -1,17 +1,17 @@
-const { Op } = require('sequelize');
-const InvoiceHeader = require('../models/InvoiceHeader');
-const InvoiceItem = require('../models/InvoiceItem');
-const User = require('../models/User');
-const Address = require('../models/Address');
-const Product = require('../models/Product');
-const DeliveryOption = require('../models/DeliveryOption');
-const PaymentProof = require('../models/PaymentProof');
-const Message = require('../models/Message');
+const { Op } = require("sequelize");
+const InvoiceHeader = require("../models/InvoiceHeader");
+const InvoiceItem = require("../models/InvoiceItem");
+const User = require("../models/User");
+const Address = require("../models/Address");
+const Product = require("../models/Product");
+const DeliveryOption = require("../models/DeliveryOption");
+const PaymentProof = require("../models/PaymentProof");
+const Message = require("../models/Message");
 
-const { startOfDay, endOfDay } = require('date-fns');
-const transporter = require('../configs/nodemailer');
-const { generatePdf } = require('../configs/puppeteer');
-const path = require('path');
+const { startOfDay, endOfDay } = require("date-fns");
+const transporter = require("../configs/nodemailer");
+const { generatePdf } = require("../configs/puppeteer");
+const path = require("path");
 
 module.exports = {
   getTransaction: async (req, res) => {
@@ -21,30 +21,35 @@ module.exports = {
       const limit = 5;
       const offset = (page - 1) * limit;
 
-      const query = { order: [['createdAt', 'DESC']], limit, offset };
+      const { keyword } = req.query;
+
+      const query = { order: [["createdAt", "DESC"]], limit, offset };
 
       if (sort) {
-        query.order = [sort.split(',')];
+        query.order = [sort.split(",")];
       }
 
       if (startDate || endDate) {
         query.where = {
           ...query.where,
           createdAt: {
-            [Op.between]: [startOfDay(new Date(startDate)), endOfDay(new Date(endDate))],
+            [Op.between]: [
+              startOfDay(new Date(startDate)),
+              endOfDay(new Date(endDate)),
+            ],
           },
         };
       }
 
-      if (search) {
+      if (keyword) {
         query.where = {
           ...query.where,
           [Op.or]: {
-            status: { [Op.substring]: search },
-            notes: { [Op.substring]: search },
-            '$user.name$': { [Op.substring]: search },
-            '$address.address$': { [Op.substring]: search },
-            '$deliveryoption.name$': { [Op.substring]: search },
+            status: { [Op.substring]: keyword },
+            notes: { [Op.substring]: keyword },
+            "$user.name$": { [Op.substring]: keyword },
+            "$address.address$": { [Op.substring]: keyword },
+            "$deliveryoption.name$": { [Op.substring]: keyword },
           },
         };
       }
@@ -58,8 +63,14 @@ module.exports = {
           { model: PaymentProof },
           {
             model: InvoiceItem,
-            attributes: ['price', 'quantity'],
-            include: [{ model: Product, attributes: ['name', 'image'], paranoid: false }],
+            attributes: ["price", "quantity"],
+            include: [
+              {
+                model: Product,
+                attributes: ["name", "image"],
+                paranoid: false,
+              },
+            ],
           },
         ],
         distinct: true,
@@ -82,19 +93,39 @@ module.exports = {
         include: [
           {
             model: InvoiceItem,
-            attributes: ['price', 'quantity', 'subtotal'],
-            include: [{ model: Product, attributes: ['name', 'image', 'unit'], paranoid: false }],
+            attributes: ["price", "quantity", "subtotal"],
+            include: [
+              {
+                model: Product,
+                attributes: ["name", "image", "unit"],
+                paranoid: false,
+              },
+            ],
           },
-          { model: User, attributes: ['name', 'email', 'phone_number'] },
-          { model: Address, attributes: ['address', 'city', 'province', 'country', 'postalcode'], paranoid: false },
-          { model: DeliveryOption, attributes: ['name', 'cost'], paranoid: false },
+          { model: User, attributes: ["name", "email", "phone_number"] },
+          {
+            model: Address,
+            attributes: [
+              "address",
+              "city",
+              "province",
+              "country",
+              "postalcode",
+            ],
+            paranoid: false,
+          },
+          {
+            model: DeliveryOption,
+            attributes: ["name", "cost"],
+            paranoid: false,
+          },
         ],
       });
 
-      if (transaction.status === 'pending') {
+      if (transaction.status === "pending") {
         await InvoiceHeader.update(
           {
-            status: 'approved',
+            status: "approved",
           },
           {
             where: { id: req.params.id },
@@ -103,7 +134,7 @@ module.exports = {
 
         await Message.create({
           userId: transaction.userId,
-          to: 'user',
+          to: "user",
           adminId: id,
           header: `Payment Approved for Invoice #${transaction.id}`,
           content: `Hello, ${transaction.user.name}!|We have approved the payment you've made for Invoice #${transaction.id}!|Please wait while we packed your order and shipped it to you immediately!|Thank you for shopping with us and we are looking forward for your next order :)|Regards,`,
@@ -138,7 +169,7 @@ module.exports = {
         }, 1000);
 
         res.status(200).send({
-          message: 'Invoice updated successfully!',
+          message: "Invoice updated successfully!",
           userId: transaction.userId,
         });
       }
@@ -157,16 +188,13 @@ module.exports = {
             attributes: ['price', 'quantity', 'subtotal', 'id'],
             include: [{ model: Product, attributes: ['name', 'image', 'unit'], paranoid: false }],
           },
-          { model: User, attributes: ['name', 'email', 'phone_number'] },
-          { model: Address, attributes: ['address', 'city', 'province', 'country', 'postalcode'], paranoid: false },
-          { model: DeliveryOption, attributes: ['name', 'cost'], paranoid: false },
         ],
       });
 
-      if (transaction.status === 'pending') {
+      if (transaction.status === "pending") {
         await InvoiceHeader.update(
           {
-            status: 'rejected',
+            status: "rejected",
           },
           {
             where: { id: req.params.id },
@@ -186,7 +214,7 @@ module.exports = {
         const invoicePdfPath = await generatePdf(transaction);
 
         await transporter.sendMail({
-          from: 'HeizenbergAdmin <admin@heizenbergco.com>',
+          from: "HeizenbergAdmin <admin@heizenbergco.com>",
           to: `${transaction.user.email}`,
           subject: `Payment Rejected for Invoice #${transaction.id}`,
           html: `
@@ -199,9 +227,11 @@ module.exports = {
           <p><b>The Heizen Berg Co. Admin Team</b></p>`,
           attachments: [
             {
-              filename: `${transaction.user.name.replace(' ', '')}_invoice_${transaction.id}.pdf`,
+              filename: `${transaction.user.name.replace(" ", "")}_invoice_${
+                transaction.id
+              }.pdf`,
               path: path.resolve(invoicePdfPath),
-              contentType: 'application/pdf',
+              contentType: "application/pdf",
             },
           ],
         });
@@ -211,7 +241,7 @@ module.exports = {
         await transaction.save();
 
         res.status(200).send({
-          message: 'Invoice updated successfully!',
+          message: "Invoice updated successfully!",
           userId: transaction.userId,
         });
       }
