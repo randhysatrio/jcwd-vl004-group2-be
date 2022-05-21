30f016add18f6c9b028c1aec8bd59b191e6a0133
randhysatrio <randhysatrio@gmail.com>
2022-05-22 00:12:56 +0700

    Sequelize env update, addressController & productController minor fix

--- configs/sequelize.js
+++ configs/sequelize.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   timezone: '+07:00',
   logging: false,
